ed9248fe9a23b1cfaabb67c6b6d165f7724380e2
Shadat Rahman <shadat.rahman.464@gmail.com>
2023-09-16 11:19:57 +0600

    init, data added

---
+++ lib/data/grocery_data.dart
@@ -0,0 +1,78 @@
+class GroceryData {
+  static List<Map<String, dynamic>> groceryProducts = [
+    {
+      'name': 'Apples',
+      'category': 'Fruits',
+      'price': 1.99,
+      'quantity': 10,
+      'image':
+          'https://img.freepik.com/free-photo/many-ripe-juicy-red-apples-covered-with-water-drops-closeup-selective-focus-ripe-fruits-as-background_166373-2611.jpg?w=2000',
+    },
+    {
+      'name': 'Bananas',
+      'category': 'Fruits',
+      'price': 0.79,
+      'quantity': 15,
+      'image': 'https://foodandnutrition.org/wp-content/uploads/SavorBananas.jpg',
+    },
+    {
+      'name': 'Milk',
+      'category': 'Dairy',
+      'price': 2.49,
+      'quantity': 3,
+      'image': 'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2016/10/how-to-make-cashew-milk-dairy-free-nut-milk-6.jpg',
+    },
+    {
+      'name': 'Bread',
+      'category': 'Bakery',
+      'price': 2.99,
+      'quantity': 5,
+      'image':
+          'https://cdn.apartmenttherapy.info/image/upload/f_jpg,q_auto:eco,c_fill,g_auto,w_1500,ar_1:1/k%2FPhoto%2FRecipe%20Ramp%20Up%2F2021-11-Potato-Bread%2Fpotato_bread_01',
+    },
+    {
+      'name': 'Eggs',
+      'category': 'Dairy',
+      'price': 1.69,
+      'quantity': 2,
+      'image': 'https://thebigmansworld.com/wp-content/uploads/2022/03/best-egg-substitute2.jpeg',
+    },
+    {
+      'name': 'Chicken',
+      'category': 'Meat',
+      'price': 5.99,
+      'quantity': 8,
+      'image':
+          'https://www.allrecipes.com/thmb/SoBuPU73KcbYHl3Kp3j8Xx4A3fc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/8805-CrispyFriedChicken-mfs-3x2-072-d55b8406d4ae45709fcdeb58a04143c2.jpg',
+    },
+    {
+      'name': 'Rice',
+      'category': 'Grains',
+      'price': 3.49,
+      'quantity': 10,
+      'image':
+          'https://www.thespruceeats.com/thmb/kUoSjpktuKuTQpxkm-OFgOxl8s4=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/how-to-make-basic-white-rice-2355883-10-5b0da96eba6177003622896e.jpg',
+    },
+    {
+      'name': 'Pasta',
+      'category': 'Grains',
+      'price': 1.29,
+      'quantity': 7,
+      'image': 'https://rainbowplantlife.com/wp-content/uploads/2021/06/sausage-pasta-lazy-meals-1-of-3-scaled.jpg',
+    },
+    {
+      'name': 'Tomatoes',
+      'category': 'Vegetables',
+      'price': 0.99,
+      'quantity': 12,
+      'image': 'https://t4.ftcdn.net/jpg/00/69/28/27/360_F_69282769_nnGX7SidAFQs8SwUgmZFx5Zlz6sXRkl4.jpg',
+    },
+    {
+      'name': 'Cereal',
+      'category': 'Breakfast',
+      'price': 3.99,
+      'quantity': 4,
+      'image': 'https://img.jamieoliver.com/jamieoliver/recipe-database/46287886.jpg?tr=w-800,h-600',
+    },
+  ];
+}
